8b4e1c7d22b7bc1421440aca1a1f318b79204926
unknown <clovis.souza2011@hotmail.com>
2020-12-29 00:27:58 -0300

    Subindo versão

---
+++ src/users/users.controller.ts
@@ -0,0 +1,21 @@
+/* eslint-disable prettier/prettier */
+import { Controller, Post, Body,ValidationPipe } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UsersService } from './users.service';
+import { ReturnUserDto } from './dto/return-user.dto';
+
+@Controller('users')
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Post('createAdminUser')
+  async createAdminUser(
+    @Body(ValidationPipe) createUserDto: CreateUserDto,
+  ): Promise<ReturnUserDto> {
+    const user = await this.usersService.createAdminUser(createUserDto);
+    return {
+      user,
+      message: 'Administrador cadastrado com sucesso',
+    };
+  }
+}
